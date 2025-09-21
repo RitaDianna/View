0cdf8694a690752558e89f628e35ae0a3eebac6d
Dianna <catrinadk@outlook.com>
2025-09-21 16:19:20 +0800

    创建View项目

---
+++ View/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  View
+//
+//  Created by Kianna on 2025/9/21.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+
+@end
+
---
+++ View/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  View
+//
+//  Created by Kianna on 2025/9/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
